d387cde233e9a81e2e44015ee5a989180ce907f8
Michael Ferger <mferger92@gmail.com>
2016-08-11 23:05:38 -0600

    added env file for secret and line in gitignore for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'b140e2fda4bd640f4354c0fc0bce1d1f039579ae9dc7b40448fbb826b498c6adde45ce5e96b5eef9cc81bf9fbf218a1f7c8f0e022ded2c5c15095a116cf4903e'
